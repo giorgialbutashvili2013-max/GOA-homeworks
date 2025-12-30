7f88756a94f305859155830453ba45e1944e2257
giorgialbutashvili2013-max <giorgialbutashvili2013@gmai.com>
2025-12-30 19:27:52 +0400

    second commit

---
+++ day 015/homework/phython.py
@@ -0,0 +1,21 @@
+slising-მუშაობს სტრინგზე
+
+
+cars=["bmw","mersedec","porshe","ford","opel","nissani"]
+print(3:5)
+
+
+name=("giorgi")
+print(1:4)
+
+
+name=("giorgi")
+print(0:2)
+print(3:5)
+
+
+name=("gio")
+    if name start with "r":
+print("hello")
+    else:
+print("bye")
---
+++ day 017/homework/python.py
@@ -0,0 +1,32 @@
+#len-რამდენი ელემენტიც არის იმდენს გამოიტანს
+#.append-ამატებს გადაცემულ ელემენტს სიის ბოლოში
+#.pop-აქრობს სიიდან ელემენტს რომელსაც მივუთითბთ
+
+
+
+numbers=[1,2,3,4,5]
+    print(numbers(::-1))
+
+
+
+nums=[1,2,3,4,5,6]
+    print(nums(.remove 6))
+    print(nums(.insert 12))
+
+
+
+nums=[1,2,3,4,5]
+    print(nums(.insert 7))
+
+
+fruits=["apple","watermalon","mango"]
+    print(fruits(.insert orange))
+
+
+
+
+
+
+
+
+
